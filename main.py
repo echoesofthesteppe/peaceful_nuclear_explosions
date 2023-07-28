3afc92851fd2f284e9659e57714477c60922312b
echoesofthesteppe <62980641+echoesofthesteppe@users.noreply.github.com>
2023-07-28 14:36:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+main = Flask(__name__)
 
-@app.route('/')
+@main.route('/')
 def index():
     return render_template('peaceful_nuclear_explosions.html')
